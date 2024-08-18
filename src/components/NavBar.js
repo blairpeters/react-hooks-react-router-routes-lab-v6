055d606c1187417d382e4287f5f21c4e70cbf399
Tony Blair Maina <blairpeters13@gmail.com>
2024-08-18 22:32:06 +0300

    completes lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,17 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/" key="home">
+        Home
+      </NavLink>
+      <NavLink to="/directors" key="directors">
+        Directors
+      </NavLink>
+      <NavLink to="/actors" key="actors">
+        Actors
+      </NavLink>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
